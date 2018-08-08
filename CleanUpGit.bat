e16aec7e22978b9f821a927d0c01ecc405631f49
G\Matt <matt@geeks.ltd.uk>
2018-08-08 10:45:28 +0100

    no message

--- CleanUpGit.bat
+++ CleanUpGit.bat
@@ -1,18 +1,20 @@
 @echo off
-for %%b in ('git branch') do (
-echo Processing %b%
-
+for /F %%b in ('git branch') do (
+echo Processing %%b
+pause
 echo Started cleaning the changes.
-git reset --hard origin/%b%
+git reset --hard origin/%%b
 echo Finished cleaning the changes.
-
+pause
 echo packages >> .gitignore
 git add .gitignore
+pause
 git commit -m "added ignorable files/directories"
-
+pause
 git rm -r --cached .
+pause
 git add .
-
+pause
 git commit -am "Removed the ignored files"
 git push
 )
